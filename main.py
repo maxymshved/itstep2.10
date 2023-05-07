b4a0052d801e9ff1c120de08bc6ed9df542f5800
p32 <ostik19791108@gmail.com>
2023-05-07 13:37:29 +0300

    print

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print ('hi world')
-
+# me_dict = {
+#     "macbook": "pro",
+#     "tesla": 'x'
+# }
+# #print(me_dict)
+# key = "Honda"
+# value = 'cv'
+# me_dict [key] = value
+# print(me_dict)
+# thisdick = dict(name = "John" , age = 36, country = "Norway")
+# print(thisdick)

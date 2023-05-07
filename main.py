5c357e3dd25ea2f3bd2f4a74bb3d01f98df4f9b7
p32 <ostik19791108@gmail.com>
2023-05-07 14:47:52 +0300

    print

--- main.py
+++ main.py
@@ -8,4 +8,46 @@
 # me_dict [key] = value
 # print(me_dict)
 # thisdick = dict(name = "John" , age = 36, country = "Norway")
+# for x in thisdick:
+#     print(thisdick[x])
 # print(thisdick)
+# for x,y in thisdick.items():
+#     print(x,y)
+# thisdict = {
+#     'brand' : 'ford',
+#     'electric': False,
+#     'years': 1964,
+#     'colors': ["red", "white", "blue"]
+# }
+# my_dict = thisdict
+# print(my_dict)
+# my_dict['new_key'] = 'new_value'
+# print(thisdict)
+# print(my_dict)
+# my_dict = {'Nick': {
+#     'phone': "1111",
+#     'insta': "2113"
+# },
+#     'Anna': {'phone': "1121",
+#     'insta': "2123"
+#
+#     }, 'Rick': {
+# 'phone': "1311",
+#     'insta': "2133"
+#     }
+#     }
+# user = my_dict['Anna']['phone']
+# print(user)
+
+
+
+my_dict = {
+'Nick': {
+'phone': '2121212',
+'insta': '@n14'
+    }
+}
+user =input(),  my_dict
+print(user)
+
+
